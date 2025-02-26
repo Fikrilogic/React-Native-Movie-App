e94017601898aaac0ac8abbd7cd4547de5e12123
Fikri Harya Sandi <fikri.sandi@ms.mii.co.id>
2025-02-27 00:40:00 +0700

    add sample unit testing

---
+++ __tests__/repository/UserRepository.test.tsx
@@ -0,0 +1,128 @@
+import {
+  MovieRepository,
+  MovieRepositoryImpl,
+} from '../../src/data/repository/MovieRepository';
+import {ApiClient, ApiClientImpl} from '../../src/data/api/ApiClient';
+import {Movie, MovieFavorite, MovieResponse} from '../../src/domain/models/Movie';
+import {DB, open, Scalar} from '@op-engineering/op-sqlite';
+
+jest.mock('../../src/data/api/ApiClient', () => {
+  return {
+    ApiClientImpl: jest.fn().mockImplementation(
+      (): ApiClient => ({
+        Get: jest.fn(),
+        Post: jest.fn(),
+        Update: jest.fn(),
+        Delete: jest.fn(),
+      }),
+    ),
+  };
+});
+
+jest.mock('@op-engineering/op-sqlite', () => {
+  return {
+    open: jest.fn(
+      (params: {name: string}): DB => ({
+        close: jest.fn(),
+        delete: jest.fn(),
+        attach: jest.fn(),
+        detach: jest.fn(),
+        transaction: jest.fn(),
+        executeSync: jest.fn(),
+        execute: jest.fn(),
+        executeWithHostObjects: jest.fn(),
+        executeBatch: jest.fn(),
+        loadFile: jest.fn(),
+        updateHook: jest.fn(),
+        commitHook: jest.fn(),
+        rollbackHook: jest.fn(),
+        prepareStatement: jest.fn(),
+        loadExtension: jest.fn(),
+        executeRaw: jest.fn(),
+        getDbPath: jest.fn(),
+        reactiveExecute: jest.fn(),
+        sync: jest.fn(),
+      }),
+    ),
+  };
+});
+
+describe('MovieRepository', () => {
+  let apiClientMock: jest.Mocked<ApiClient>;
+  let db: jest.Mocked<DB>;
+  let repository: MovieRepository;
+  const mockMoviePopular = new MovieResponse(
+    [
+      new Movie({
+        title: 'test title',
+        backdrop_path: 'image',
+        popularity: 10,
+        poster_path: 'poster image',
+        overview: 'this is overview',
+      }),
+      new Movie({
+        title: 'test title',
+        backdrop_path: 'image',
+        popularity: 10,
+        poster_path: 'poster image',
+        overview: 'this is overview',
+      }),
+      new Movie({
+        title: 'test title',
+        backdrop_path: 'image',
+        popularity: 10,
+        poster_path: 'poster image',
+        overview: 'this is overview',
+      }),
+      new Movie({
+        title: 'test title',
+        backdrop_path: 'image',
+        popularity: 10,
+        poster_path: 'poster image',
+        overview: 'this is overview',
+      }),
+      new Movie({
+        title: 'test title',
+        backdrop_path: 'image',
+        popularity: 10,
+        poster_path: 'poster image',
+        overview: 'this is overview',
+      }),
+    ],
+    10,
+    1,
+    1,
+  );
+
+  const mockMovieFavorites: MovieFavorite[] =  [
+    new MovieFavorite({
+      id: 1,
+      title: 'movie test favorite',
+      poster_path: 'img url',
+      overview: 'test movie overview',
+      is_favorite: true
+    })
+  ]
+
+  beforeEach(() => {
+    apiClientMock = ApiClientImpl() as jest.Mocked<ApiClient>;
+    db = open({name: 'test'}) as jest.Mocked<DB>;
+    repository = MovieRepositoryImpl(apiClientMock, db);
+  });
+
+  it('must get movie', async () => {
+    apiClientMock.Get.mockResolvedValue(mockMoviePopular);
+
+    const res = await repository.getPopular();
+    expect(res).toEqual(mockMoviePopular);
+  });
+
+  it('must get error get movie', async () => {
+    apiClientMock.Get.mockRejectedValue(new Error('movie'));
+    await expect(repository.getPopular()).rejects.toThrow('movie');
+  });
+
+  // it('must get movie popular', async () => {
+    // db.execute.mockResolvedValue(mockMovieFavorites)
+  // })
+});
